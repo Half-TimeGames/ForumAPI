bb4ee1044ac6ced51f4452e703c9d01306f69bba
Andreas Dellrud <andreas.dellrud@hotmail.com>
2015-11-18 19:40:19 +0100

    Fixed PM entity

--- ForumAPI/Entities/PrivateMessage.cs
+++ ForumAPI/Entities/PrivateMessage.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace Entities
 {
     public class PrivateMessage
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public User SenderUser { get; set; }
+        public User ReceiverUser { get; set; }
+        public string Text { get; set; }
     }
 }
