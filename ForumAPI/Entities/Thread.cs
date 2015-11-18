987c2157e5b251afd63f80ddbc6529c50e30b93f
Andreas Dellrud <andreas.dellrud@hotmail.com>
2015-11-18 19:37:21 +0100

    Skapat skit....

---
+++ ForumAPI/Entities/Thread.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Thread
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public Topic Topic { get; set; }
+        public string Subject { get; set; }
+        public ICollection<Post> Posts { get; set; } 
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ ForumAPI/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public DateTime DateCreateed { get; set; }
+        public string Avatar { get; set; }
+    }
+}
